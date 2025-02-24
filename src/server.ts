59e889fb0c58b53e754694b59f04c61e1f4c5de7
tech.julian.salvador <tech.julian.salvador@gmail.com>
2025-02-24 14:04:57 +0800

    feat(server): config express \& ejs middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import configureEJS from "./middlewares/ejs-middleware";
+import expressConfig from "./middlewares/express-middleware";
 
 const PORT = 3000;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("HARO WADO");
-});
+// EJS templating configuration
+configureEJS(app);
+
+// Express middleware configuration
+expressConfig(app);
 
 app.listen(PORT, () => console.log(`Server running in port: ${PORT}`));
