95c15c1187a388f72d27fc61bbddaaee8a8b9036
tech.julian.salvador <tech.julian.salvador@gmail.com>
2025-02-24 21:31:31 +0800

    fix(middlewares): fix minor path configuration for views dir

--- src/middlewares/ejs-middleware.ts
+++ src/middlewares/ejs-middleware.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default function configureEJS(app: Express) {
   // set default templating to ejs
-  app.set("views engine", "ejs");
+  app.set("view engine", "ejs");
 
   // define the path to the views directory
   app.set("views", path.join(_src, "views"));
