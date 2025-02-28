a2f4f652c059b83cea7bc6843c0a51b56d1beb34
tech.julian.salvador <tech.julian.salvador@gmail.com>
2025-02-28 10:57:59 +0800

    feat(server): add words route

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import configureEJS from "./middlewares/ejs-middleware";
 import expressConfig from "./middlewares/express-middleware";
 import { app_routes } from "./app/routes";
+import { word_routes } from "./app/words/routes";
 
 const PORT = 3000;
 const app = express();
@@ -13,5 +14,6 @@ configureEJS(app);
 expressConfig(app);
 
 app.use(app_routes);
+app.use("/words", word_routes);
 
 app.listen(PORT, () => console.log(`Server running in port: ${PORT}`));
